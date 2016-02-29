86c2b3e786df335aaf97b41ff6ca2c16382e85f3
icecoolcat <icecoolcat@users.noreply.github.com>
2016-02-29 16:06:03 +0800

    version 0.0.1

--- app/src/main/java/com/example/android/mdpandroid/Bluetooth/DeviceListActivity.java
+++ app/src/main/java/com/example/android/mdpandroid/Bluetooth/DeviceListActivity.java
@@ -1,6 +1,6 @@
-package com.example.android.mdpandroid;
 
-import java.util.Set;
+package com.example.android.mdpandroid.Bluetooth;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -9,16 +9,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
+
+import com.example.android.mdpandroid.R;
+import com.example.android.mdpandroid.logger.Log;
+
+import java.util.Set;
 
 /**
  * This Activity appears as a dialog. It lists any paired devices and
@@ -26,18 +28,26 @@ import android.widget.AdapterView.OnItemClickListener;
  * by the user, the MAC address of the device is sent back to the parent
  * Activity in the result Intent.
  */
-
 public class DeviceListActivity extends Activity {
-    // Debugging
-    private static final String TAG = "DeviceListActivity";
-    private static final boolean D = true;
 
-    // Return Intent extra
+    /**
+     * Tag for Log
+     */
+    private static final String TAG = "DeviceListActivity";
+
+    /**
+     * Return Intent extra
+     */
     public static String EXTRA_DEVICE_ADDRESS = "device_address";
 
-    // Member fields
+    /**
+     * Member fields
+     */
     private BluetoothAdapter mBtAdapter;
-    private ArrayAdapter<String> mPairedDevicesArrayAdapter;
+
+    /**
+     * Newly discovered devices
+     */
     private ArrayAdapter<String> mNewDevicesArrayAdapter;
 
     @Override
@@ -46,14 +56,14 @@ public class DeviceListActivity extends Activity {
 
         // Setup the window
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setContentView(R.layout.device_list);
+        setContentView(R.layout.activity_device_list);
 
         // Set result CANCELED in case the user backs out
         setResult(Activity.RESULT_CANCELED);
 
         // Initialize the button to perform device discovery
         Button scanButton = (Button) findViewById(R.id.button_scan);
-        scanButton.setOnClickListener(new OnClickListener() {
+        scanButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 doDiscovery();
                 v.setVisibility(View.GONE);
@@ -62,12 +72,13 @@ public class DeviceListActivity extends Activity {
 
         // Initialize array adapters. One for already paired devices and
         // one for newly discovered devices
-        mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+        ArrayAdapter<String> pairedDevicesArrayAdapter =
+                new ArrayAdapter<String>(this, R.layout.device_name);
         mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
 
         // Find and set up the ListView for paired devices
         ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
-        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
+        pairedListView.setAdapter(pairedDevicesArrayAdapter);
         pairedListView.setOnItemClickListener(mDeviceClickListener);
 
         // Find and set up the ListView for newly discovered devices
@@ -93,11 +104,11 @@ public class DeviceListActivity extends Activity {
         if (pairedDevices.size() > 0) {
             findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
             for (BluetoothDevice device : pairedDevices) {
-                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                pairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
             }
         } else {
             String noDevices = getResources().getText(R.string.none_paired).toString();
-            mPairedDevicesArrayAdapter.add(noDevices);
+            pairedDevicesArrayAdapter.add(noDevices);
         }
     }
 
@@ -118,7 +129,7 @@ public class DeviceListActivity extends Activity {
      * Start device discover with the BluetoothAdapter
      */
     private void doDiscovery() {
-        if (D) Log.d(TAG, "doDiscovery()");
+        Log.d(TAG, "doDiscovery()");
 
         // Indicate scanning in the title
         setProgressBarIndeterminateVisibility(true);
@@ -136,8 +147,11 @@ public class DeviceListActivity extends Activity {
         mBtAdapter.startDiscovery();
     }
 
-    // The on-click listener for all devices in the ListViews
-    private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
+    /**
+     * The on-click listener for all devices in the ListViews
+     */
+    private AdapterView.OnItemClickListener mDeviceClickListener
+            = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
             // Cancel discovery because it's costly and we're about to connect
             mBtAdapter.cancelDiscovery();
@@ -156,8 +170,10 @@ public class DeviceListActivity extends Activity {
         }
     };
 
-    // The BroadcastReceiver that listens for discovered devices and
-    // changes the title when discovery is finished
+    /**
+     * The BroadcastReceiver that listens for discovered devices and changes the title when
+     * discovery is finished
+     */
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
--- app/src/main/java/com/example/android/mdpandroid/MainActivity.java
+++ app/src/main/java/com/example/android/mdpandroid/MainActivity.java
@@ -13,6 +13,8 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -41,9 +43,14 @@ import android.hardware.SensorEventListener;
 import android.view.MotionEvent;
 import android.view.View.OnTouchListener;
 import android.widget.RelativeLayout;
+
+import com.example.android.mdpandroid.Bluetooth.BluetoothMainActivity;
+import com.example.android.mdpandroid.Bluetooth.BluetoothService;
+import com.example.android.mdpandroid.Bluetooth.DeviceListActivity;
+
 import java.util.ArrayList;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     // Message types sent from the BluetoothChatService Handler
     public static final int MESSAGE_STATE_CHANGE = 1;
@@ -136,13 +143,11 @@ public class MainActivity extends Activity implements SensorEventListener {
                     mConversationArrayAdapter.add("Me:  " + writeMessage);  //the message that appears on top of the "send" button
                     mOutEditText.setText("");
                     break;
-                case MESSAGE_READ:
-                    byte[] readBuf = (byte[]) msg.obj;                      //the status of the robotView
-                    // construct a string from the valid bytes in the buffer
+                case MESSAGE_READ:// construct a string from the valid bytes in the buffer
                     String readMessage = new String(readBuf, 0, msg.arg1);
                     TextView text = (TextView) findViewById(R.id.tb_status);
 
-                    if (readMessage.startsWith(getString(R.string.start_STATUS))) {
+                    if (readMessage.startsWith(getString(R.string.start_STATUS))) {     //status below the joyStick
                         text.setText(readMessage);
                     } else if (readMessage.contains(getString(R.string.robot_status))) {
                         text.setText(readMessage.substring(11, readMessage.length() - 2));
@@ -201,7 +206,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         // ----------------------------JoyStick----------------------------------
         layout_joystick = (RelativeLayout) findViewById(R.id.layout_joystick);
@@ -549,7 +555,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         mOutStringBuffer = new StringBuffer("");
 
         //Initialise Explore and Shortest Path btn
-        exploreButton = (Button) findViewById(R.id.btn_explore);
+        exploreButton = (Button) findViewById(R.id.explore);
         exploreButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -557,7 +563,7 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         });
 
-        runshortestButton = (Button) findViewById(R.id.btn_run);
+        runshortestButton = (Button) findViewById(R.id.runButton);
         runshortestButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -591,7 +597,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         });
         */
 
-        tiltToggle = (ToggleButton) findViewById(R.id.tilt_toogle);
+        tiltToggle = (ToggleButton) findViewById(R.id.tilt_toggle);
         tiltToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -926,24 +932,45 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent serverIntent = null;
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                return true;
-
             case R.id.action_bluetooth:
-                Intent intent = new Intent(this, BluetoothSettings.class);
+<<<<<<< HEAD
+                // Launch the DeviceListActivity to see devices and do scan
+                serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent,REQUEST_CONNECT_DEVICE_INSECURE);
+                break;
+=======
+                Intent intent = new Intent(this, BluetoothMainActivity.class);
                 startActivity(intent);
 
                 // as a favorite...
                 return true;
-
+            case R.id.secure_connect_scan: {
+                // Launch the DeviceListActivity to see devices and do scan
+                Intent serverIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+                return true;
+            }
+            case R.id.insecure_connect_scan: {
+                // Launch the DeviceListActivity to see devices and do scan
+                Intent serverIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+                return true;
+            }
+            case R.id.discoverable: {
+                // Ensure this device is discoverable by others
+                ensureDiscoverable();
+                return true;
+            }
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
+>>>>>>> dd471872ddd3820d0b438dfe09bca1cf31bec0ab
         }
+        return false;
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -1013,6 +1040,18 @@ public class MainActivity extends Activity implements SensorEventListener {
             displayView(position);  //position is an int
         }
     }
+    /**
+     * Makes this device discoverable.
+     */
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
 }
 
 
